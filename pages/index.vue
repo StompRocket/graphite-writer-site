bdff202635903d98a3e7e2028f9a3e5821953466
Ronan <rsf.sho@gmail.com>
2018-12-26 13:59:54 -0800

    added features section

--- pages/index.vue
+++ pages/index.vue
@@ -6,17 +6,40 @@
       </a>
       <a href="#pricing">Pricing</a>
       <a href="#about">About</a>
-      <a href="#about">Older Versions</a>
+      <a href="https://legacy.graphitewriter.com">Older Versions</a>
       <a href="https://graphite.stomprocket.io" class="btn yellow">Login</a>
     </nav>
     <main class="homePage" id="top">
       <div class="coverImage">
         <h1 class="tagLine">School note taking, <br> made easy</h1>
-        <a  class="btn cta" href="https://graphite.stomprocket.io">Sign Up</a>
-        <a class="legacyNotice" href="https://legacy.graphitewriter.com">Looking for older versions of Graphite Writer?</a>
+        <a class="btn cta" href="https://graphite.stomprocket.io">Sign Up</a>
+        <a class="legacyNotice" href="https://legacy.graphitewriter.com">Looking for older versions of Graphite
+          Writer?</a>
       </div>
       <div class="features">
-        features
+        <div class="header">
+          <h1>Designed For Students</h1>
+          <p class="text">Graphite Writer not only organizes notes by class, it uses an intelligent search system that lets you find
+            notes with ease.</p>
+          <p class="scroll">scroll to learn more</p>
+        </div>
+        <div class="feature class">
+          <h2>Day Of Class</h2>
+          <p>Graphite Writer lets you search by which day you had class. No longer do you have to sift through pages of
+            notes just to figure out what you did last Monday.</p>
+
+        </div>
+        <div class="feature topics">
+          <h2>Topics</h2>
+          <p>Graphite Writer’s intelligent search finds keywords and topics in your notes.</p>
+        </div>
+        <div class="feature sync">
+          <h2>Real Time Sync</h2>
+          <p>Graphite Writer automatically saves your notes as you type, preventing loss of data.</p>
+        </div>
+        <div class="featureImg">
+
+        </div>
       </div>
       <div class="free">free</div>
       <div class="fast">fast</div>
